f49aca53efcff1e50785e013174c2417f77a64d6
suconghou <1126045770@qq.com>
2016-08-30 15:24:54 +0800

    update libev 2.5.0

--- shadowsocks/shadowsocks-libev.sh
+++ shadowsocks/shadowsocks-libev.sh
@@ -1,7 +1,7 @@
 apk update && apk upgrade
 apk --update add gcc g++ make curl  zlib-dev openssl-dev linux-headers asciidoc xmlto
 cd /tmp
-SHADOWSOCKS_LIBEV_VERSION=2.4.8
+SHADOWSOCKS_LIBEV_VERSION=2.5.0
 CPU_NUM=`cat /proc/cpuinfo | grep processor | wc -l`
 curl -sSL https://github.com/shadowsocks/shadowsocks-libev/archive/v${SHADOWSOCKS_LIBEV_VERSION}.tar.gz |  tar xz
 cd shadowsocks-libev-${SHADOWSOCKS_LIBEV_VERSION}
