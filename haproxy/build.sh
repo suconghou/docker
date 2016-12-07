6c266b2edf37c4b4e2c768130d09528df9847fa7
suconghou <1126045770@qq.com>
2016-12-07 22:27:01 +0800

    update redis version and haproxy version

--- haproxy/build.sh
+++ haproxy/build.sh
@@ -1,9 +1,9 @@
 apk update && apk upgrade
 apk --update add gcc g++ make wget linux-headers
 cd /tmp
-HAPROXY_VERSION=haproxy-1.6.9
+HAPROXY_VERSION=haproxy-1.7.0
 CPU_NUM=`cat /proc/cpuinfo | grep processor | wc -l`
-wget http://www.haproxy.org/download/1.6/src/${HAPROXY_VERSION}.tar.gz
+wget http://www.haproxy.org/download/1.7/src/${HAPROXY_VERSION}.tar.gz
 tar zxf ${HAPROXY_VERSION}.tar.gz
 cd ${HAPROXY_VERSION}
 export CFLAGS="-O3"
