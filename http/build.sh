5ce595221268b9a3f20d9e5b2ac05839035681d8
suconghou <1126045770@qq.com>
2017-07-13 15:14:09 +0800

    update

--- http/build.sh
+++
@@ -1,2 +0,0 @@
-GO_ENABLED=0 GOOS=linux GOARCH=amd64 go build  -a -ldflags  "-s -w" http.go
-
--- http/http.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "net/http"
-
-func main() {
-	panic(http.ListenAndServe(":8080", http.FileServer(http.Dir("./"))))
-}
