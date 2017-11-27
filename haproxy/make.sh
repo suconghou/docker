2e8095d13f11d5c952c4596387a60f01125f6dc1
suconghou <1126045770@qq.com>
2017-11-27 11:01:42 +0800

    update haproxy 1.8.0

--- haproxy/make.sh
+++ haproxy/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t=suconghou/haproxy -t=suconghou/haproxy:1.7.9 -t=suconghou/haproxy:alpine . && \
+docker build -t=suconghou/haproxy -t=suconghou/haproxy:1.8.0 -t=suconghou/haproxy:alpine . && \
 docker build -t=suconghou/haproxy:busybox . -f Dockerfile-busybox
