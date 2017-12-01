5fc167a295ba9e8bd1d9170c67ec022fb8e268bc
suconghou <1126045770@qq.com>
2017-12-01 15:02:49 +0800

    update redis 4.0.4

--- redis/make.sh
+++ redis/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t=suconghou/redis -t=suconghou/redis:4.0.2 -t=suconghou/redis:alpine . && \
+docker build -t=suconghou/redis -t=suconghou/redis:4.0.4 -t=suconghou/redis:alpine . && \
 docker build -t=suconghou/redis:busybox . -f Dockerfile-busybox
