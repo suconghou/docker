9602045f9a5ae1b7c4518a9af7cc284b39cc8155
suconghou <1126045770@qq.com>
2018-01-24 19:17:42 +0800

    save

--- nginx-google/make.sh
+++ nginx-google/make.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-docker pull alpine
-BUILDSH=$(cat build.sh)
-docker run -it --name tmpbuild alpine sh -c "$BUILDSH"
-rm -f ./nginx.tar.gz
-docker cp tmpbuild:/nginx.tar.gz ./nginx.tar.gz
-docker rm tmpbuild
-docker build -t=suconghou/nginx-google .
+docker build -t=suconghou/nginx-google . 
+
