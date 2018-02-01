23496956fdbc8296ec3bc4ff6e34505c27ab9029
suconghou <1126045770@qq.com>
2018-02-01 22:43:01 +0800

    up

--- php/make.sh
+++ php/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t=suconghou/php -t=suconghou/php:7.2.1 . && \
+docker build -t=suconghou/php -t=suconghou/php:7.2.2 . && \
 docker build -t=suconghou/php:cli . -f Dockerfile-cli && \
 docker build -t=suconghou/php:fpm . -f Dockerfile-fpm
