fd5f7007b4aa1d943bf9d2e233a296ffafbedb2c
suconghou <1126045770@qq.com>
2018-02-09 19:04:48 +0800

    up

---
+++ polipo/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+
+polipo chunkHighMark=$chunkHighMark objectHighMark=$objectHighMark pmmFirstSize=$pmmFirstSize pmmSize=$pmmSize proxyPort=$proxyPort socksParentProxy=0.0.0.0:$socksPort socksProxyType=$socksProxyType
