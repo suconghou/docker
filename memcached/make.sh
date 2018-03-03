352202a10cb2385fb3f5264287cc79d5feaeccad
suconghou <1126045770@qq.com>
2018-03-03 23:26:51 +0800

    up

--- memcached/make.sh
+++ memcached/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t=suconghou/memcached -t=suconghou/memcached:1.5.5 -t=suconghou/memcached:alpine . && \
+docker build -t=suconghou/memcached -t=suconghou/memcached:1.5.6 -t=suconghou/memcached:alpine . && \
 docker build -t=suconghou/memcached:busybox . -f Dockerfile-busybox
