4677a0ee554c9ad63090b0457a4d0ea880807c91
suconghou <1126045770@qq.com>
2018-03-28 20:33:19 +0800

    save

--- redis/make.sh
+++ redis/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t=suconghou/redis -t=suconghou/redis:4.0.8 -t=suconghou/redis:alpine . && \
+docker build -t=suconghou/redis -t=suconghou/redis:4.0.9 -t=suconghou/redis:alpine . && \
 docker build -t=suconghou/redis:busybox . -f Dockerfile-busybox
