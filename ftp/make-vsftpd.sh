6a91fb118b7d15ef80b4c67d7265db0b6edec1e6
suconghou <1126045770@qq.com>
2018-04-13 16:09:50 +0800

    fix wrong version

--- ftp/make-vsftpd.sh
+++ ftp/make-vsftpd.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/ftp:vsftpd -t=suconghou/ftp:vsftpd-3.03 . -f Dockerfile-vsftpd
+docker build -t=suconghou/ftp:vsftpd -t=suconghou/ftp:vsftpd-3.0.3 . -f Dockerfile-vsftpd
