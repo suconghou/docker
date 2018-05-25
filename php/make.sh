20d73b24321c8e68bf56d1ca7796d83a6063125d
suconghou <1126045770@qq.com>
2018-05-25 15:48:12 +0800

    update php node

--- php/make.sh
+++ php/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t=suconghou/php -t=suconghou/php:7.2.5 . && \
+docker build -t=suconghou/php -t=suconghou/php:7.2.6 . && \
 docker build -t=suconghou/php:cli . -f Dockerfile-cli && \
 docker build -t=suconghou/php:fpm . -f Dockerfile-fpm
