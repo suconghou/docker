bdb7f8421dcd5dcd84fc53289c1504823b487407
suconghou <1126045770@qq.com>
2018-07-10 16:58:11 +0800

    add puppeteer

--- memcached/make.sh
+++ memcached/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t=suconghou/memcached -t=suconghou/memcached:1.5.8 -t=suconghou/memcached:alpine . && \
+docker build -t=suconghou/memcached -t=suconghou/memcached:1.5.9 -t=suconghou/memcached:alpine . && \
 docker build -t=suconghou/memcached:busybox . -f Dockerfile-busybox
