476659a03c665ef56343fcf7651ca852728709cc
suconghou <1126045770@qq.com>
2018-11-07 09:49:25 +0800

    update nginx

--- nginx/make.sh
+++ nginx/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t=suconghou/nginx -t=suconghou/nginx:1.15.5 -t=suconghou/nginx:alpine . && \
+docker build -t=suconghou/nginx -t=suconghou/nginx:1.15.6 -t=suconghou/nginx:alpine . && \
 docker build -t=suconghou/nginx:busybox . -f Dockerfile-busybox
 
