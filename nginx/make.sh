dc6b3496530c9f9a9bde644a4a2b40f641dac367
suconghou <1126045770@qq.com>
2019-04-01 14:42:53 +0800

    up

--- nginx/make.sh
+++ nginx/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t=suconghou/nginx -t=suconghou/nginx:1.15.9 -t=suconghou/nginx:alpine . && \
+docker build -t=suconghou/nginx -t=suconghou/nginx:1.15.10 -t=suconghou/nginx:alpine . && \
 docker build -t=suconghou/nginx:busybox . -f Dockerfile-busybox
 
