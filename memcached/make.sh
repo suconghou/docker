a8161659efa69f9d131761f2e54253a0c73c5c66
suconghou <1126045770@qq.com>
2019-04-17 10:28:44 +0800

    up memcached nginx

--- memcached/make.sh
+++ memcached/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t=suconghou/memcached -t=suconghou/memcached:1.5.12 -t=suconghou/memcached:alpine . && \
+docker build -t=suconghou/memcached -t=suconghou/memcached:1.5.13 -t=suconghou/memcached:alpine . && \
 docker build -t=suconghou/memcached:busybox . -f Dockerfile-busybox
--- nginx/make.sh
+++ nginx/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t=suconghou/nginx -t=suconghou/nginx:1.15.11 -t=suconghou/nginx:alpine . && \
+docker build -t=suconghou/nginx -t=suconghou/nginx:1.15.12 -t=suconghou/nginx:alpine . && \
 docker build -t=suconghou/nginx:busybox . -f Dockerfile-busybox
 
