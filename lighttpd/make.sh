15d06c0085938b356368d2590d9b097c3f0a1c6d
suconghou <1126045770@qq.com>
2019-05-29 22:11:07 +0800

    influx

--- lighttpd/make.sh
+++ lighttpd/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t=suconghou/lighttpd -t=suconghou/lighttpd:1.4.49 -t=suconghou/lighttpd:alpine . && \
+docker build -t=suconghou/lighttpd -t=suconghou/lighttpd:1.4.54 -t=suconghou/lighttpd:alpine . && \
 docker build -t=suconghou/lighttpd:busybox . -f Dockerfile-busybox
