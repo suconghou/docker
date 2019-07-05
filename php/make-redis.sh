ab014c839eff259338e228b9317b8cb4f5205fff
suconghou <1126045770@qq.com>
2019-07-05 12:50:57 +0800

    update php version

--- php/make-redis.sh
+++ php/make-redis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:redis -t=suconghou/php:7.3.6_redis . -f Dockerfile-redis
+docker build -t=suconghou/php:redis -t=suconghou/php:7.3.7_redis . -f Dockerfile-redis
--- php/make-swoole.sh
+++ php/make-swoole.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:swoole -t=suconghou/php:7.3.6_swoole_4.3.5 . -f Dockerfile-swoole
+docker build -t=suconghou/php:swoole -t=suconghou/php:7.3.7_swoole_4.3.5 . -f Dockerfile-swoole
