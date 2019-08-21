be1783f3e9259c0cc2a543be88b236d89937bbf6
suconghou <1126045770@qq.com>
2019-08-21 13:12:20 +0800

    update php dockerfile

--- php/72/make-72-swoole.sh
+++ php/72/make-72-swoole.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:7.2.21_swoole_4.4.2 . -f Dockerfile-72-swoole
+docker build -t=suconghou/php:7.2.21_swoole_4.4.4 . -f Dockerfile-72-swoole
--- php/make-swoole.sh
+++ php/make-swoole.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:swoole -t=suconghou/php:7.3.8_swoole_4.4.2 . -f Dockerfile-swoole
+docker build -t=suconghou/php:swoole -t=suconghou/php:7.3.8_swoole_4.4.4 . -f Dockerfile-swoole
