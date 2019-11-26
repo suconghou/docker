38d17911ee4bf69ffc789caf16640cb4d427fbed
suconghou <1126045770@qq.com>
2019-11-26 13:02:37 +0800

    fix: update php , node

--- node/make.sh
+++ node/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t=suconghou/node -t=suconghou/node:13.1.0  .
+docker build -t=suconghou/node -t=suconghou/node:13.2.0  .
 
--- php/72/make-72-swoole.sh
+++ php/72/make-72-swoole.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:7.2.24_swoole_4.4.12 . -f Dockerfile-72-swoole
+docker build -t=suconghou/php:7.2.25_swoole_4.4.12 . -f Dockerfile-72-swoole
