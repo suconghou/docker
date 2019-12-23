253f959f9a0ec650c2350dd1b93a1bf834b8aafa
suconghou <1126045770@qq.com>
2019-12-23 15:23:33 +0800

    fix : up node

--- node/make.sh
+++ node/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t=suconghou/node -t=suconghou/node:13.4.0  .
+docker build -t=suconghou/node -t=suconghou/node:13.5.0  .
 
--- php/73/make.sh
+++ php/73/make.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php -t=suconghou/php:7.3.13 . 
+docker build -t=suconghou/php:7.3.13 . 
