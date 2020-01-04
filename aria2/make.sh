37d73547f3859bd27198a162b9edc21db9f0eb31
suconghou <1126045770@qq.com>
2020-01-04 14:38:51 +0800

    fix : build aria2

--- aria2/make.sh
+++ aria2/make.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --add-host github-production-release-asset-2e65be.s3.amazonaws.com:103.79.76.128 --add-host github.com:103.79.76.128 -t=suconghou/aria2 .
+docker build -t=suconghou/aria2 .
