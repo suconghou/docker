673cead250cc851eae3248bf296aeb48e46d4964
suconghou <1126045770@qq.com>
2020-02-12 11:18:24 +0800

    update .

--- php/make-swoole.sh
+++ php/make-swoole.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:swoole -t=suconghou/php:7.4.1_swoole_4.4.15 . -f Dockerfile-swoole
+docker build -t=suconghou/php:swoole -t=suconghou/php:7.4.2_swoole_4.4.15 . -f Dockerfile-swoole
