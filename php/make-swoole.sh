2a184c8dbb1791ff0d81752da51d9fcb57acc3af
suconghou <1126045770@qq.com>
2020-06-12 14:03:13 +0800

    fix: update php node

--- php/make-swoole.sh
+++ php/make-swoole.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:swoole -t=suconghou/php:7.4.6_swoole_4.5.1 . -f Dockerfile-swoole
+docker build -t=suconghou/php:swoole -t=suconghou/php:7.4.7_swoole_4.5.2 . -f Dockerfile-swoole
