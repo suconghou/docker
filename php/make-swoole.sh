adb655371fff46e03f61501b3242157269651a60
suconghou <1126045770@qq.com>
2020-07-10 15:35:54 +0800

    fix:update php nginx

--- php/make-swoole.sh
+++ php/make-swoole.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:swoole -t=suconghou/php:7.4.7_swoole_4.5.2 . -f Dockerfile-swoole
+docker build -t=suconghou/php:swoole -t=suconghou/php:7.4.8_swoole_4.5.2 . -f Dockerfile-swoole
