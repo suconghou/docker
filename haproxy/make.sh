f8f93076d5114721fd10b9317301acfafdcad6b8
suconghou <1126045770@qq.com>
2020-08-31 12:58:41 +0800

    fix: update php

--- haproxy/make.sh
+++ haproxy/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t=suconghou/haproxy -t=suconghou/haproxy:2.2.1 -t=suconghou/haproxy:alpine . && \
+docker build -t=suconghou/haproxy -t=suconghou/haproxy:2.2.2 -t=suconghou/haproxy:alpine . && \
 docker build -t=suconghou/haproxy:busybox . -f Dockerfile-busybox
