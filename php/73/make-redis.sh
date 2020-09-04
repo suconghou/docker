283099a5fe889acbc47e01b35543fabd49f2174d
suconghou <1126045770@qq.com>
2020-09-04 09:58:19 +0800

    up php

--- php/73/make-redis.sh
+++ php/73/make-redis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:7.3.21_redis . -f Dockerfile-redis
+docker build -t=suconghou/php:7.3.22_redis . -f Dockerfile-redis
--- php/make-swoole.sh
+++ php/make-swoole.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:swoole -t=suconghou/php:7.4.9_swoole_4.5.3 . -f Dockerfile-swoole
+docker build -t=suconghou/php:swoole -t=suconghou/php:7.4.10_swoole_4.5.3 . -f Dockerfile-swoole
