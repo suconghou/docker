872973076ed850c3e60a793deacacc77f26c777f
suconghou <1126045770@qq.com>
2021-04-22 09:45:51 +0800

    fix: update nginx

--- nginx/make.sh
+++ nginx/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t=suconghou/nginx -t=suconghou/nginx:1.19.9 -t=suconghou/nginx:alpine . && \
+docker build -t=suconghou/nginx -t=suconghou/nginx:1.20.0 -t=suconghou/nginx:alpine . && \
 docker build -t=suconghou/nginx:busybox . -f Dockerfile-busybox
 
