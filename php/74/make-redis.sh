6eb09390b2a21c49486b188159b24feb1db7d3f6
suconghou <1126045770@qq.com>
2021-08-02 09:32:51 +0800

    fix: php

--- php/74/make-redis.sh
+++ php/74/make-redis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:7.4.21_redis . -f Dockerfile-redis
+docker build -t=suconghou/php:7.4.22_redis . -f Dockerfile-redis
--- php/make.sh
+++ php/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t=suconghou/php -t=suconghou/php:8.0.8 . && \
+docker build -t=suconghou/php -t=suconghou/php:8.0.9 . && \
 docker build -t=suconghou/php:cli . -f Dockerfile-cli && \
 docker build -t=suconghou/php:fpm . -f Dockerfile-fpm
