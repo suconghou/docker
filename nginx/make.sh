41c6f5670b4b5718cfe78aa36765dea9d3d3b863
suconghou <1126045770@qq.com>
2021-09-14 22:36:31 +0800

    fix: save

--- nginx/make.sh
+++ nginx/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t=suconghou/nginx -t=suconghou/nginx:1.21.1 -t=suconghou/nginx:alpine . && \
+docker build -t=suconghou/nginx -t=suconghou/nginx:1.21.3 -t=suconghou/nginx:alpine . && \
 docker build -t=suconghou/nginx:busybox . -f Dockerfile-busybox
 
