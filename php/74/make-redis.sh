6a53cf38fabdea935872b2a26fb187f933a181f1
suconghou <1126045770@qq.com>
2021-09-29 16:06:50 +0800

    fix: update php

--- php/74/make-redis.sh
+++ php/74/make-redis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:7.4.22_redis . -f Dockerfile-redis
+docker build -t=suconghou/php:7.4.24_redis . -f Dockerfile-redis
--- php/74/make-swoole.sh
+++ php/74/make-swoole.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:7.4.22_swoole_4.7.1 . -f Dockerfile-swoole
+docker build -t=suconghou/php:7.4.24_swoole_4.7.1 . -f Dockerfile-swoole
