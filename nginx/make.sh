0e572a165d9f6c62d5fb84a7c90cbdd6b00c9c4e
sucongohu <1126045770@qq.com>
2022-01-12 10:03:56 +0800

    update swoole

--- nginx/make.sh
+++ nginx/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t=suconghou/nginx -t=suconghou/nginx:1.21.4 -t=suconghou/nginx:alpine . && \
+docker build -t=suconghou/nginx -t=suconghou/nginx:1.21.5 -t=suconghou/nginx:alpine . && \
 docker build -t=suconghou/nginx:busybox . -f Dockerfile-busybox
 
--- php/make-swoole.sh
+++ php/make-swoole.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:swoole -t=suconghou/php:8.1.1_swoole_4.8.5 . -f Dockerfile-swoole
+docker build -t=suconghou/php:swoole -t=suconghou/php:8.1.1_swoole_4.8.6 . -f Dockerfile-swoole
