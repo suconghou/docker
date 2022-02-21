bbc114e627a422b1c00859c047e29bb21df2a19d
sucongohu <1126045770@qq.com>
2022-02-21 11:19:18 +0800

    fix:update swoole

--- php/make-swoole.sh
+++ php/make-swoole.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:swoole -t=suconghou/php:8.1.3_swoole_4.8.6 . -f Dockerfile-swoole
+docker build -t=suconghou/php:swoole -t=suconghou/php:8.1.3_swoole_4.8.7 . -f Dockerfile-swoole
