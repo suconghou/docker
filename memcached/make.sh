3b53affaabc50976cf79e8d3efe2ca73c34e1d77
sucongohu <1126045770@qq.com>
2022-05-23 22:57:36 +0800

    fix: php mongodb

--- memcached/make.sh
+++ memcached/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t=suconghou/memcached -t=suconghou/memcached:1.6.12 -t=suconghou/memcached:alpine . && \
+docker build -t=suconghou/memcached -t=suconghou/memcached:1.6.15 -t=suconghou/memcached:alpine . && \
 docker build -t=suconghou/memcached:busybox . -f Dockerfile-busybox
