b1de9c16fef3a0ca3f8ff352bc4510cdb82e7dbb
sucongohu <1126045770@qq.com>
2022-08-02 14:02:48 +0800

    update swoole

--- nginx/make.sh
+++ nginx/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t=suconghou/nginx -t=suconghou/nginx:1.23.0 -t=suconghou/nginx:alpine . && \
+docker build -t=suconghou/nginx -t=suconghou/nginx:1.23.1 -t=suconghou/nginx:alpine . && \
 docker build -t=suconghou/nginx:busybox . -f Dockerfile-busybox
 
