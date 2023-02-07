4f0e67509b14c27b232b4750a5f1c7e618c45d53
sucongohu <1126045770@qq.com>
2023-02-07 14:24:01 +0800

    update php

--- php/81/make-redis.sh
+++ php/81/make-redis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:8.1.14_redis . -f Dockerfile-redis
+docker build -t=suconghou/php:8.1.15_redis . -f Dockerfile-redis
--- php/81/make-swoole.sh
+++ php/81/make-swoole.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:8.1.14_swoole_5.0.1 . -f Dockerfile-swoole
+docker build -t=suconghou/php:8.1.15_swoole_5.0.2 . -f Dockerfile-swoole
