3148f0a1b5026fbe0728f41b2073182fc79eb374
sucongohu <1126045770@qq.com>
2023-07-24 13:50:48 +0800

    update node

--- redis/make.sh
+++ redis/make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t=suconghou/redis -t=suconghou/redis:7.0.10 -t=suconghou/redis:alpine . && \
+docker build -t=suconghou/redis -t=suconghou/redis:7.0.12 -t=suconghou/redis:alpine . && \
 docker build -t=suconghou/redis:busybox . -f Dockerfile-busybox
