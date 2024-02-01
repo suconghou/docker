7108a092c4160e38ed299bd0f24f2596343e98b8
sucongohu <1126045770@qq.com>
2024-02-01 13:55:46 +0800

    save

--- openresty/make.sh
+++ openresty/make.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/openresty -t=suconghou/openresty:1.21.4.1  . 
+docker build -t=suconghou/openresty -t=suconghou/openresty:1.25.3.1  . 
--- php/82/make-redis.sh
+++ php/82/make-redis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t=suconghou/php:8.2.14_redis . -f Dockerfile-redis
+docker build -t=suconghou/php:8.2.15_redis . -f Dockerfile-redis
